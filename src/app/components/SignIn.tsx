a158b91614b743eb5fd0a6864d96bc7f49882a61
Anatoly Arkhipov <atol83@mail.ru>
2022-05-07 00:21:10 +0300

    fix: fix Copyright

--- src/app/components/SignIn.tsx
+++ src/app/components/SignIn.tsx
@@ -22,8 +22,8 @@ function Copyright() {
   return (
     <Typography variant="body2" color="text.secondary" align="center" sx={{ mt: 8, mb: 4 }}>
       {'Copyright © '}
-      <Link color="inherit" href="https://mui.com/">
-        Your Website
+      <Link color="inherit" href="#">
+        TEMPER
       </Link>{' '}
       {new Date().getFullYear()}
       {'.'}
--- src/app/components/SignUp.tsx
+++ src/app/components/SignUp.tsx
@@ -24,8 +24,8 @@ function Copyright() {
   return (
     <Typography variant="body2" color="text.secondary" align="center" sx={{ mt: 5 }}>
       {'Copyright © '}
-      <Link color="inherit" href="https://mui.com/">
-        Your Website
+      <Link color="inherit" href="#">
+        TEMPER
       </Link>{' '}
       {new Date().getFullYear()}
       {'.'}
