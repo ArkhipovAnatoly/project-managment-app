2bd69a5367bfdb487305471b84d0e0841a7251bc
Anatoly Arkhipov <atol83@mail.ru>
2022-05-07 13:31:00 +0300

    feat: add routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
+import Header from './app/components/share/Header';
+import SignIn from './app/components/SignIn';
+import SignUp from './app/components/SignUp';
 
 function App() {
-  return <></>;
+  return (
+    <Router>
+      <Header />
+      <Routes>
+        <Route path="/signin" element={<SignIn />} />
+        <Route path="/signup" element={<SignUp />} />
+      </Routes>
+    </Router>
+  );
 }
-
 export default App;
