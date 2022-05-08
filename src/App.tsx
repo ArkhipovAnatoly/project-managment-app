8f0f3bb367f7c89e87871fb6439fe16c4ff6e1ed
Anatoly Arkhipov <atol83@mail.ru>
2022-05-08 23:00:48 +0300

    feat: add pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,24 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
-import Header from './app/components/share/Header';
-import SignIn from './app/components/SignIn';
-import SignUp from './app/components/SignUp';
+import AppLayout from './app/components/layouts/AppLayout';
+import WelcomePage from './pages/WelcomePage/WelcomePage';
+import Error from './pages/Error/Error';
+import SignIn from './pages/SingIn/SignIn';
+import SignUp from './pages/SignUp/SignUp';
 
 function App() {
   return (
     <Router>
-      <Header />
       <Routes>
-        <Route path="/signin" element={<SignIn />} />
-        <Route path="/signup" element={<SignUp />} />
+        <Route path="/" element={<AppLayout />}>
+          <Route index element={<WelcomePage />} />
+          <Route path="/signin" element={<SignIn />} />
+          <Route path="/signup" element={<SignUp />} />
+        </Route>
+        <Route path="*" element={<Error />} />
       </Routes>
     </Router>
   );
 }
+
 export default App;
