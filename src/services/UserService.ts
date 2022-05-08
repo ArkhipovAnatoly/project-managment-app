fa3c0ed8dd1ecc6dcb42a80206075460ef09b90f
Anatoly Arkhipov <atol83@mail.ru>
2022-05-08 19:14:10 +0300

    fix: reverse mutation generics

--- src/services/UserService.ts
+++ src/services/UserService.ts
@@ -1,18 +1,18 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/dist/query/react';
-import { UserSignInData, UserSignUpData } from '../types';
+import { SignInResponse, SignUpResponse, UserSignInData, UserSignUpData } from '../types';
 
 export const userAPI = createApi({
   reducerPath: 'userAPI',
   baseQuery: fetchBaseQuery({ baseUrl: 'https://react-app-kanban.herokuapp.com' }),
   endpoints: (build) => ({
-    userSignIn: build.mutation<UserSignInData, UserSignInData>({
+    userSignIn: build.mutation<SignInResponse, UserSignInData>({
       query: (userData) => ({
         url: '/signin',
         method: 'POST',
         body: userData,
       }),
     }),
-    userSignUp: build.mutation<UserSignUpData, UserSignUpData>({
+    userSignUp: build.mutation<SignUpResponse, UserSignUpData>({
       query: (userData) => ({
         url: '/signup',
         method: 'POST',
