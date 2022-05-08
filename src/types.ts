243025304913d2a143ab65473cad5ea7b197d678
Anatoly Arkhipov <atol83@mail.ru>
2022-05-08 19:14:38 +0300

    feat: add resonse's type

--- src/types.ts
+++ src/types.ts
@@ -8,3 +8,31 @@ export type UserSignUpData = {
   login: string;
   password: string;
 };
+
+export type SignInResponse = {
+  error?: {
+    data: {
+      message: string;
+      statusCode: number;
+    };
+    status: number;
+  };
+  data?: {
+    token: string;
+  };
+};
+
+export type SignUpResponse = {
+  error?: {
+    data: {
+      message: string;
+      statusCode: number;
+    };
+    status: number;
+  };
+  data?: {
+    id: string;
+    login: string;
+    name: string;
+  };
+};
