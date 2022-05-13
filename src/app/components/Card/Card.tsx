bd0f49c49926a6213587998b3714b7448a2f9521
Anatoly Arkhipov <atol83@mail.ru>
2022-05-14 00:38:13 +0300

    fix: card props

--- src/app/components/Card/Card.tsx
+++ src/app/components/Card/Card.tsx
@@ -1,4 +1,3 @@
-import * as React from 'react';
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
@@ -19,7 +18,7 @@ export default function ActionAreaCard(props: CardProps) {
       }}
     >
       <CardActionArea>
-        <CardMedia component="img" height="300px" image={props.imgSrc} alt="green iguana" />
+        <CardMedia component="img" height="300" image={props.imgSrc} alt={props.name} />
         <CardContent>
           <Typography gutterBottom variant="h5" component="div">
             {props.name}
