b52ccd50b1d434186a25748e3130798c43c86e0d
Anatoly Arkhipov <atol83@mail.ru>
2022-05-13 22:59:50 +0300

    refactor:  remove no need imports

--- src/app/components/layouts/AppLayout.tsx
+++ src/app/components/layouts/AppLayout.tsx
@@ -1,11 +1,10 @@
 import { Outlet } from 'react-router-dom';
-import Footer from '../share/Footer/Footer';
 
 const AppLayout = () => {
   return (
     <>
       <Outlet />
-      {/* сюда подставится дочерний роут с path=”/” или “/about” */}
+      {}
     </>
   );
 };
--- src/pages/WelcomePage/WelcomePage.tsx
+++ src/pages/WelcomePage/WelcomePage.tsx
@@ -5,6 +5,7 @@ import { Box, Modal, Typography } from '@mui/material';
 import Card from '../../app/components/Card/Card';
 import CustomizedButton from '../../app/components/share/Button/CustomizedButton';
 import { useAppSelector } from '../../app/hooks';
+import Footer from '../../app/components/share/Footer/Footer';
 
 const WelcomePage = () => {
   const [videoModalActive, setVideomodalactive] = useState(false);
@@ -21,69 +22,72 @@ const WelcomePage = () => {
 
   return (
     <div className="wrapper">
-      {auth.isAuth ? (
-        <div className="autorizationBtns">
-          <CustomizedButton innerText={'Go to Main Page'} link={'/main'} />
+      <main className="main">
+        {auth.isAuth ? (
+          <div className="autorizationBtns">
+            <CustomizedButton innerText={'Go to Main page'} link={'/main'} />
+          </div>
+        ) : (
+          <div className="autorizationBtns">
+            <CustomizedButton innerText={'Sign in'} link={'/signin'} />
+            <CustomizedButton innerText={'Sign up'} link={'/signup'} />
+          </div>
+        )}
+
+        <div className="aboutTheProject">
+          <h1>
+            Название <span className="titleProject">TEMPER</span>
+          </h1>
+          <p className="title">О проекте:</p>
+          <p className="title">Здесь нужно</p>
+          <p className="title">будет добавить</p>
+          <p className="title">инфу о нашем</p>
+          <p className="title">проекте</p>
         </div>
-      ) : (
-        <div className="autorizationBtns">
-          <CustomizedButton innerText={'Sign in'} link={'/signin'} />
-          <CustomizedButton innerText={'Sign up'} link={'/signup'} />
-        </div>
-      )}
-
-      <div className="aboutTheProject">
-        <h1>
-          Название <span className="titleProject">TEMPER</span>
-        </h1>
-        <p className="title">О проекте:</p>
-        <p className="title">Здесь нужно</p>
-        <p className="title">будет добавить</p>
-        <p className="title">инфу о нашем</p>
-        <p className="title">проекте</p>
-      </div>
-      <div className="videoPlaceholder">
-        <a
-          data-video="https://player.vimeo.com/video/174002812"
-          href="#video"
-          aria-controls="videoModal"
-          onClick={openModal}
-        >
-          <img
-            className="hasShadow"
-            src={require('../../assets/img/videoPlaceholder.jpg')}
-            alt="Hero"
-            width={896}
-            height={504}
-          />
-        </a>
-      </div>
-      <Modal id="videoModal" open={videoModalActive} onClose={closeModal}>
-        <Box>
-          <Typography id="modal-modal-title" variant="h6" component="h2"></Typography>
-          <Typography id="modal-modal-description" sx={{ mt: 2 }}>
-            <iframe src="https://player.vimeo.com/video/174002812%22%3E"></iframe>
-          </Typography>
-        </Box>
-      </Modal>
-
-      <div className="aboutTheComand">
-        <h2>О команде</h2>
-        <p className="title">
-          &#8222; В командной работе хорошо то, что с вами всегда есть кто-то ещё &#8220;
-        </p>
-
-        <div className="cards">
-          {data.map((item, index) => (
-            <Card
-              key={index}
-              imgSrc={item.imgSrc}
-              name={item.name}
-              description={item.description}
+        <div className="videoPlaceholder">
+          <a
+            data-video="https://player.vimeo.com/video/174002812"
+            href="#video"
+            aria-controls="videoModal"
+            onClick={openModal}
+          >
+            <img
+              className="hasShadow"
+              src={require('../../assets/img/videoPlaceholder.jpg')}
+              alt="Hero"
+              width={896}
+              height={504}
             />
-          ))}
+          </a>
         </div>
-      </div>
+        <Modal id="videoModal" open={videoModalActive} onClose={closeModal}>
+          <Box>
+            <Typography id="modal-modal-title" variant="h6" component="h2"></Typography>
+            <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+              <iframe src="https://player.vimeo.com/video/174002812%22%3E"></iframe>
+            </Typography>
+          </Box>
+        </Modal>
+
+        <div className="aboutTheComand">
+          <h2>О команде</h2>
+          <p className="title">
+            &#8222; В командной работе хорошо то, что с вами всегда есть кто-то ещё &#8220;
+          </p>
+
+          <div className="cards">
+            {data.map((item, index) => (
+              <Card
+                key={index}
+                imgSrc={item.imgSrc}
+                name={item.name}
+                description={item.description}
+              />
+            ))}
+          </div>
+        </div>
+      </main>
+      <Footer />
     </div>
   );
 };
