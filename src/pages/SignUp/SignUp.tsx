07c3533e4b12ad39f8a34cca8c18053abf5f5859
Anatoly Arkhipov <atol83@mail.ru>
2022-05-14 16:15:09 +0300

    feat: add show animation

--- src/pages/SignUp/SignUp.tsx
+++ src/pages/SignUp/SignUp.tsx
@@ -13,6 +13,7 @@ import {
   Typography,
   FormHelperText,
   CircularProgress,
+  Grow,
 } from '@mui/material';
 
 import Copyright from '../../app/components/share/Copyright';
@@ -117,134 +118,144 @@ export default function SignUp() {
 
   return (
     <Box sx={{ display: 'flex', flexDirection: 'column', height: '100%' }}>
-      <Container component="section" sx={{ backgroundColor: 'white', marginTop: 8 }} maxWidth="xs">
-        {isShowForm && (
-          <>
-            <Box
-              sx={{
-                display: 'flex',
-                flexDirection: 'column',
-                alignItems: 'center',
-              }}
-            >
-              <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
-                <PersonAddAltIcon />
-              </Avatar>
-              <Typography component="h1" variant="h5">
-                Sign up
-              </Typography>
+      <Grow
+        style={{ transformOrigin: '0 0 0' }}
+        in={isShowForm}
+        {...(isShowForm ? { timeout: 1000 } : {})}
+      >
+        <Container
+          component="section"
+          sx={{ backgroundColor: 'white', marginTop: 8 }}
+          maxWidth="xs"
+        >
+          {isShowForm && (
+            <>
+              <Box
+                sx={{
+                  display: 'flex',
+                  flexDirection: 'column',
+                  alignItems: 'center',
+                }}
+              >
+                <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+                  <PersonAddAltIcon />
+                </Avatar>
+                <Typography component="h1" variant="h5">
+                  Sign up
+                </Typography>
 
-              <Box component="form" noValidate onSubmit={handleSubmit(onSubmit)} sx={{ mt: 3 }}>
-                <Grid container spacing={2}>
-                  <Grid item xs={12}>
-                    <TextField
-                      error={errors.name && true}
-                      autoComplete="given-name"
-                      required
-                      fullWidth
-                      id="Name"
-                      label="Name"
-                      autoFocus
-                      {...register('name', { required: true, pattern: /^[A-Za-zА-Яа-я]+$/i })}
-                    />
-                    {errors.name?.type === 'required' && (
-                      <FormHelperText component="span" error>
-                        Name is required
-                      </FormHelperText>
-                    )}
-                    {errors.name?.type === 'pattern' && (
-                      <FormHelperText component="span" error>
-                        Name should include letters only
-                      </FormHelperText>
-                    )}
+                <Box component="form" noValidate onSubmit={handleSubmit(onSubmit)} sx={{ mt: 3 }}>
+                  <Grid container spacing={2}>
+                    <Grid item xs={12}>
+                      <TextField
+                        error={errors.name && true}
+                        autoComplete="given-name"
+                        required
+                        fullWidth
+                        id="Name"
+                        label="Name"
+                        autoFocus
+                        {...register('name', { required: true, pattern: /^[A-Za-zА-Яа-я]+$/i })}
+                      />
+                      {errors.name?.type === 'required' && (
+                        <FormHelperText component="span" error>
+                          Name is required
+                        </FormHelperText>
+                      )}
+                      {errors.name?.type === 'pattern' && (
+                        <FormHelperText component="span" error>
+                          Name should include letters only
+                        </FormHelperText>
+                      )}
+                    </Grid>
+
+                    <Grid item xs={12}>
+                      <TextField
+                        error={errors.login && true}
+                        required
+                        fullWidth
+                        id="login"
+                        label="Login"
+                        autoComplete="login"
+                        {...register('login', {
+                          required: true,
+                        })}
+                      />
+                      {errors.login?.type === 'required' && (
+                        <FormHelperText component="span" error>
+                          Login is required
+                        </FormHelperText>
+                      )}
+                    </Grid>
+                    <Grid item xs={12}>
+                      <TextField
+                        error={errors.password && true}
+                        required
+                        fullWidth
+                        label="Password"
+                        type="password"
+                        id="password"
+                        autoComplete="new-password"
+                        {...register('password', { required: true, minLength: 8 })}
+                      />
+                      {errors.password?.type === 'required' && (
+                        <FormHelperText component="span" error>
+                          Password is required
+                        </FormHelperText>
+                      )}
+                      {errors.password?.type === 'minLength' && (
+                        <FormHelperText component="span" error>
+                          Password length should be more than 8 characters
+                        </FormHelperText>
+                      )}
+                    </Grid>
                   </Grid>
 
-                  <Grid item xs={12}>
-                    <TextField
-                      error={errors.login && true}
-                      required
-                      fullWidth
-                      id="login"
-                      label="Login"
-                      autoComplete="login"
-                      {...register('login', {
-                        required: true,
-                      })}
-                    />
-                    {errors.login?.type === 'required' && (
-                      <FormHelperText component="span" error>
-                        Login is required
+                  <Box
+                    sx={{
+                      mt: 2,
+                      display: 'flex',
+                      justifyContent: 'center',
+                    }}
+                  >
+                    {isLoading && <CircularProgress size={26} color="info" />}
+                    {
+                      <FormHelperText
+                        error={isError}
+                        component="span"
+                        sx={{
+                          color: { isSuccess } && '#00FF00',
+                          fontSize: '18px',
+                        }}
+                      >
+                        {message}
                       </FormHelperText>
-                    )}
-                  </Grid>
-                  <Grid item xs={12}>
-                    <TextField
-                      error={errors.password && true}
-                      required
-                      fullWidth
-                      label="Password"
-                      type="password"
-                      id="password"
-                      autoComplete="new-password"
-                      {...register('password', { required: true, minLength: 8 })}
-                    />
-                    {errors.password?.type === 'required' && (
-                      <FormHelperText component="span" error>
-                        Password is required
-                      </FormHelperText>
-                    )}
-                    {errors.password?.type === 'minLength' && (
-                      <FormHelperText component="span" error>
-                        Password length should be more than 8 characters
-                      </FormHelperText>
-                    )}
-                  </Grid>
-                </Grid>
+                    }
+                  </Box>
 
-                <Box
-                  sx={{
-                    mt: 2,
-                    display: 'flex',
-                    justifyContent: 'center',
-                  }}
-                >
-                  {isLoading && <CircularProgress size={26} color="info" />}
-                  {
-                    <FormHelperText
-                      error={isError}
-                      component="span"
-                      sx={{
-                        color: { isSuccess } && '#00FF00',
-                        fontSize: '18px',
-                      }}
-                    >
-                      {message}
-                    </FormHelperText>
-                  }
+                  <Button
+                    disabled={isDisabled}
+                    type="submit"
+                    fullWidth
+                    variant="contained"
+                    sx={{ mt: 3, mb: 2 }}
+                  >
+                    Sign Up
+                  </Button>
+                  <Grid container justifyContent="flex-end">
+                    <Grid item>
+                      <Link component={NavLink} to="/signin" variant="body2">
+                        Already have an account? Sign in
+                      </Link>
+                    </Grid>
+                  </Grid>
                 </Box>
-
-                <Button
-                  disabled={isDisabled}
-                  type="submit"
-                  fullWidth
-                  variant="contained"
-                  sx={{ mt: 3, mb: 2 }}
-                >
-                  Sign Up
-                </Button>
-                <Grid container justifyContent="flex-end">
-                  <Grid item>
-                    <Link component={NavLink} to="/signin" variant="body2">
-                      Already have an account? Sign in
-                    </Link>
-                  </Grid>
-                </Grid>
               </Box>
-            </Box>
-            <Copyright />
-          </>
-        )}
-      </Container>
+              <Copyright />
+            </>
+          )}
+        </Container>
+      </Grow>
       <Footer />
     </Box>
   );
--- src/pages/SingIn/SignIn.tsx
+++ src/pages/SingIn/SignIn.tsx
@@ -12,6 +12,7 @@ import {
   Typography,
   FormHelperText,
   CircularProgress,
+  Grow,
 } from '@mui/material';
 
 import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
@@ -120,107 +121,117 @@ export default function SignIn() {
 
   return (
     <Box sx={{ display: 'flex', flexDirection: 'column', height: '100%' }}>
-      <Container component="section" sx={{ backgroundColor: 'white', marginTop: 8 }} maxWidth="xs">
-        {isShowForm && (
-          <>
-            <Box
-              sx={{
-                display: 'flex',
-                flexDirection: 'column',
-                alignItems: 'center',
-              }}
-            >
-              <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
-                <LockOutlinedIcon />
-              </Avatar>
-              <Typography component="h1" variant="h5">
-                Sign in
-              </Typography>
+      <Grow
+        style={{ transformOrigin: '0 0 0' }}
+        in={isShowForm}
+        {...(isShowForm ? { timeout: 1000 } : {})}
+      >
+        <Container
+          component="section"
+          sx={{ backgroundColor: 'white', marginTop: 8 }}
+          maxWidth="xs"
+        >
+          {isShowForm && (
+            <>
+              <Box
+                sx={{
+                  display: 'flex',
+                  flexDirection: 'column',
+                  alignItems: 'center',
+                }}
+              >
+                <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+                  <LockOutlinedIcon />
+                </Avatar>
+                <Typography component="h1" variant="h5">
+                  Sign in
+                </Typography>
 
-              <Box component="form" onSubmit={handleSubmit(onSubmit)} noValidate sx={{ mt: 1 }}>
-                <TextField
-                  error={errors.login && true}
-                  margin="normal"
-                  required
-                  id="Login"
-                  label="Login"
-                  fullWidth
-                  autoComplete="given-login"
-                  autoFocus
-                  {...register('login', { required: true })}
-                />
-                {errors.login?.type === 'required' && (
-                  <FormHelperText component="span" error>
-                    Login is required
-                  </FormHelperText>
-                )}
-
-                <TextField
-                  error={errors.password && true}
-                  margin="normal"
-                  required
-                  fullWidth
-                  label="Password"
-                  type="password"
-                  id="password"
-                  autoComplete="current-password"
-                  {...register('password', { required: true, minLength: 8 })}
-                />
-                {errors.password?.type === 'required' && (
-                  <FormHelperText component="span" error>
-                    Password is required
-                  </FormHelperText>
-                )}
-                {errors.password?.type === 'minLength' && (
-                  <FormHelperText component="span" error>
-                    Password length should be more than 8 characters
-                  </FormHelperText>
-                )}
-                <Box
-                  sx={{
-                    mt: 2,
-                    display: 'flex',
-                    justifyContent: 'center',
-                  }}
-                >
-                  {isLoading && <CircularProgress size={26} color="info" />}
-                  {
-                    <FormHelperText
-                      error={isError}
-                      component="span"
-                      sx={{
-                        color: { isSuccess } && '#00FF00',
-                        fontSize: '18px',
-                      }}
-                    >
-                      {message}
+                <Box component="form" onSubmit={handleSubmit(onSubmit)} noValidate sx={{ mt: 1 }}>
+                  <TextField
+                    error={errors.login && true}
+                    margin="normal"
+                    required
+                    id="Login"
+                    label="Login"
+                    fullWidth
+                    autoComplete="given-login"
+                    autoFocus
+                    {...register('login', { required: true })}
+                  />
+                  {errors.login?.type === 'required' && (
+                    <FormHelperText component="span" error>
+                      Login is required
                     </FormHelperText>
-                  }
-                </Box>
+                  )}
 
-                <Button
-                  disabled={isDisabled}
-                  type="submit"
-                  fullWidth
-                  variant="contained"
-                  sx={{ mt: 3, mb: 2 }}
-                >
-                  Sign In
-                </Button>
+                  <TextField
+                    error={errors.password && true}
+                    margin="normal"
+                    required
+                    fullWidth
+                    label="Password"
+                    type="password"
+                    id="password"
+                    autoComplete="current-password"
+                    {...register('password', { required: true, minLength: 8 })}
+                  />
+                  {errors.password?.type === 'required' && (
+                    <FormHelperText component="span" error>
+                      Password is required
+                    </FormHelperText>
+                  )}
+                  {errors.password?.type === 'minLength' && (
+                    <FormHelperText component="span" error>
+                      Password length should be more than 8 characters
+                    </FormHelperText>
+                  )}
+                  <Box
+                    sx={{
+                      mt: 2,
+                      display: 'flex',
+                      justifyContent: 'center',
+                    }}
+                  >
+                    {isLoading && <CircularProgress size={26} color="info" />}
+                    {
+                      <FormHelperText
+                        error={isError}
+                        component="span"
+                        sx={{
+                          color: { isSuccess } && '#00FF00',
+                          fontSize: '18px',
+                        }}
+                      >
+                        {message}
+                      </FormHelperText>
+                    }
+                  </Box>
 
-                <Grid container>
-                  <Grid item>
-                    <Link component={NavLink} to="/signup" variant="body2">
-                      {"Don't have an account? Sign Up"}
-                    </Link>
+                  <Button
+                    disabled={isDisabled}
+                    type="submit"
+                    fullWidth
+                    variant="contained"
+                    sx={{ mt: 3, mb: 2 }}
+                  >
+                    Sign In
+                  </Button>
+
+                  <Grid container>
+                    <Grid item>
+                      <Link component={NavLink} to="/signup" variant="body2">
+                        {"Don't have an account? Sign Up"}
+                      </Link>
+                    </Grid>
                   </Grid>
-                </Grid>
+                </Box>
               </Box>
-            </Box>
-            <Copyright />
-          </>
-        )}
-      </Container>
+              <Copyright />
+            </>
+          )}
+        </Container>
+      </Grow>
       <Footer />
     </Box>
   );
