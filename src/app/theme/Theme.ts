a5875bc4f0fb14ee23bccb7e074be96893b6d21d
Anatoly Arkhipov <atol83@mail.ru>
2022-05-20 19:56:43 +0300

    fix: headerTheme

--- src/app/theme/Theme.ts
+++ src/app/theme/Theme.ts
@@ -1,41 +1,16 @@
-import { createTheme, alpha } from '@mui/material';
-import {} from '@mui/material/colors';
-
-const headerTheme = createTheme({
-  palette: {
-    white: {
-      main: '#fff',
-    },
-    appBarColor: {
-      main: alpha('#151719', 1),
-      contrastText: '#fff',
-    },
-    appBarColorScroll: {
-      main: alpha('#151719', 0.5),
-      contrastText: '#fff',
-    },
-  },
-});
+import { createTheme, alpha, PaletteMode } from '@mui/material';
+import { amber, indigo } from '@mui/material/colors';
 
 declare module '@mui/material/styles' {
   interface Palette {
-    white: Palette['primary'];
     appBarColor: Palette['primary'];
     appBarColorScroll: Palette['primary'];
   }
 
   // allow configuration using `createTheme`
   interface PaletteOptions {
-    white?: PaletteOptions['primary'];
     appBarColor?: PaletteOptions['primary'];
     appBarColorScroll?: PaletteOptions['primary'];
-  }
-}
-
-// Update the ButtonGroup's color prop options
-declare module '@mui/material/ButtonGroup' {
-  interface ButtonGroupPropsColorOverrides {
-    white: true;
   }
 }
 
@@ -58,4 +33,32 @@ const breakpointsTheme = createTheme({
   },
 });
 
-export { headerTheme, breakpointsTheme };
+const getDesignTokens = (mode: PaletteMode) => ({
+  palette: {
+    mode,
+    ...(mode === 'dark'
+      ? {
+          warning: {
+            main: amber[900],
+            contrastText: '#fff',
+          },
+          background: {
+            default: '#151719',
+          },
+          primary: {
+            main: indigo[500],
+            dark: indigo[700],
+          },
+          secondary: {
+            main: '#26c6da',
+            contrastText: '#fff',
+          },
+          text: {
+            primary: '#fff',
+          },
+        }
+      : {}),
+  },
+});
+
+export { breakpointsTheme, getDesignTokens };
