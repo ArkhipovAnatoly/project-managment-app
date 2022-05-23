00b1fa697e7ead0e8e74d95ce7ef91ac8240d295
Anatoly Arkhipov <atol83@mail.ru>
2022-05-23 22:51:34 +0300

    feat: add return jsx depend on status

--- src/app/components/Board/Boards.tsx
+++ src/app/components/Board/Boards.tsx
@@ -1,13 +1,51 @@
+import { Box, CircularProgress, Typography, List } from '@mui/material';
+import { useTranslation } from 'react-i18next';
 import { boardAPI } from '../../../services/BoardService';
+import ConfirmModal from '../modal/ConfirmModal';
 import Board from './Board';
 
 export default function Boards() {
   const { data: boards, isLoading, isError, isSuccess } = boardAPI.useGetAllBoardsQuery('');
+  const { t } = useTranslation('board');
+
+  if (isLoading) {
+    return (
+      <Box
+        sx={{
+          mt: 2,
+          display: 'flex',
+          justifyContent: 'center',
+        }}
+      >
+        <CircularProgress size={26} color="info" />
+      </Box>
+    );
+  }
+
   return (
-    <div>
-      {boards?.map((board) => {
-        return <Board {...board} key={board.boardId} />;
-      })}
-    </div>
+    <>
+      {isSuccess && (
+        <List dense>
+          {boards?.map((board, i) => {
+            return <Board {...board} key={i} />;
+          })}
+        </List>
+      )}
+
+      {isError && (
+        <Box
+          sx={{
+            mt: 2,
+            display: 'flex',
+            justifyContent: 'center',
+          }}
+        >
+          <Typography component="h2" variant="h5">
+            {t('errorLoad')}
+          </Typography>
+        </Box>
+      )}
+      <ConfirmModal title={t('question')} type="board" />
+    </>
   );
 }
