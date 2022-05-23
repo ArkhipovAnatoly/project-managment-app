673cfff43e17b75290a176aa532a6c20fae9be4f
Anatoly Arkhipov <atol83@mail.ru>
2022-05-23 22:47:31 +0300

    feat: add tagTypes

--- src/services/BoardService.tsx
+++ src/services/BoardService.tsx
@@ -7,18 +7,22 @@ export const boardAPI = createApi({
   baseQuery: fetchBaseQuery({
     baseUrl: 'https://react-app-kanban.herokuapp.com',
     prepareHeaders: (headers: Headers, { getState }) => {
-      const token = (getState() as RootState).userAuthReducer.auth.token;
+      const token =
+        (getState() as RootState).userAuthReducer.auth.token || localStorage.getItem('token');
+
       if (token) {
         headers.set('authorization', `Bearer ${token}`);
       }
       return headers;
     },
   }),
+  tagTypes: ['Board'],
   endpoints: (build) => ({
     getAllBoards: build.query<BoardData[], ''>({
       query: () => ({
         url: '/boards',
       }),
+      providesTags: () => ['Board'],
     }),
     getBoard: build.query<BoardData, string>({
       query: (boardId) => ({
@@ -27,9 +31,9 @@ export const boardAPI = createApi({
     }),
     updateBoard: build.mutation<BoardDataResponse, BoardData>({
       query(userData) {
-        const { boardId, ...body } = userData;
+        const { id, ...body } = userData;
         return {
-          url: `/boards/${boardId}`,
+          url: `/boards/${id}`,
           method: 'PUT',
           body,
         };
@@ -43,12 +47,14 @@ export const boardAPI = createApi({
           body: boardData,
         };
       },
+      invalidatesTags: ['Board'],
     }),
     deleteBoard: build.mutation<DeleteBoardResponse, string>({
       query: (boardId) => ({
-        url: `/users/${boardId}`,
+        url: `/boards/${boardId}`,
         method: 'DELETE',
       }),
+      invalidatesTags: ['Board'],
     }),
   }),
 });
