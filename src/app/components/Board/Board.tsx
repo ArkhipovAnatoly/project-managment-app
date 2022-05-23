e3f37b0ff6cde7d98a6e9b1d0858d9b3eb049ea1
Anatoly Arkhipov <atol83@mail.ru>
2022-05-23 22:52:10 +0300

    fix: change to LIst

--- src/app/components/Board/Board.tsx
+++ src/app/components/Board/Board.tsx
@@ -1,29 +1,62 @@
-import { Accordion, AccordionDetails, AccordionSummary, Paper, Typography } from '@mui/material';
-import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import {
+  Avatar,
+  Divider,
+  Grid,
+  IconButton,
+  ListItem,
+  ListItemAvatar,
+  ListItemText,
+  Paper,
+  Typography,
+  useTheme,
+} from '@mui/material';
+
+import FolderIcon from '@mui/icons-material/Folder';
+import DeleteIcon from '@mui/icons-material/Delete';
+import { useAppDispatch } from '../../hooks';
+import { confirmModalSlice } from '../../store/reducers/ConfirmModalSlice';
 
 type BoardProps = {
-  boardId?: string;
+  id?: string;
   title: string;
 };
 
-export default function Board({ boardId, title }: BoardProps) {
+export default function Board({ id, title }: BoardProps) {
+  const theme = useTheme();
+  const { showConfirmModal } = confirmModalSlice.actions;
+  const dispatch = useAppDispatch();
+  const openModal = () => {
+    localStorage.setItem('boardId', id as string);
+    dispatch(showConfirmModal(true));
+  };
+
   return (
-    <Paper elevation={12}>
-      <Accordion>
-        <AccordionSummary
-          expandIcon={<ExpandMoreIcon />}
-          aria-controls="panel1a-content"
-          id="panel1a-header"
+    <>
+      <Grid item xs={12} md={6}>
+        <Typography sx={{ mt: 4, mb: 2 }} variant="h6" component="div">
+          {title}
+        </Typography>
+
+        <ListItem
+          sx={{ cursor: 'pointer' }}
+          secondaryAction={
+            <IconButton
+              sx={{ color: theme.palette.mode === 'dark' ? 'common.white' : 'primary.main' }}
+              edge="end"
+              aria-label="delete"
+              onClick={openModal}
+            >
+              <DeleteIcon fontSize="large" />
+            </IconButton>
+          }
         >
-          <Typography>{title}</Typography>
-        </AccordionSummary>
-        <AccordionDetails>
-          <Typography>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex,
-            sit amet blandit leo lobortis eget.
-          </Typography>
-        </AccordionDetails>
-      </Accordion>
-    </Paper>
+          <ListItemAvatar>
+            <FolderIcon color="primary" fontSize="large" />
+          </ListItemAvatar>
+          <ListItemText primary={title} />
+        </ListItem>
+        <Divider />
+      </Grid>
+    </>
   );
 }
