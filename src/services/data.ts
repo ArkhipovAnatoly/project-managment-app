d4149bef333d952f1e535849fd403a36faffb95a
Архипов Анатолий <arkhipov.a@madin.com>
2022-05-30 16:19:22 +0300

    feat: add info

--- src/services/data.ts
+++ src/services/data.ts
@@ -2,16 +2,20 @@ export default [
   {
     imgSrc: 'assets/img/avatars/Tolik.jpg',
     name: 'Анатолий - Team Lead',
-    description: 'Header, Авторизация Регистрация, Локализация',
+    descriptionEn: 'Header, SignUp, SignIn, Localization, App main route.',
+    descriptionRu: 'Header, Авторизация, Регистрация, Локализация, Главный роут приложения.',
   },
   {
     imgSrc: 'assets/img/avatars/Marina.png',
     name: 'Марина',
-    description: '- Вставим фото, - Описание,- О проделанной работе',
+    descriptionEn: 'Welcome page, App main route, animation, footer.',
+    descriptionRu: 'Домашняя страница, Главный роут приложения, Анимация, footer.',
   },
   {
     imgSrc: 'assets/img/avatars/Nikita.png',
     name: 'Никита',
-    description: '- Вставим фото, - Описание,- О проделанной работе',
+    descriptionEn: 'App Board route, drag-n-drop feature, Global search, Modal windows. ',
+    descriptionRu:
+      'Board роут приложения, Функция перетаскивания, Глобальный поиск, Модальные окна.',
   },
 ];
